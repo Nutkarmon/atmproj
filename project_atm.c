0bbee2e9ee328d134ee96676d9d81d3cce87d895
Nutkamon Manosatcharak <651435@live.hcu.ac.th>
2022-11-17 15:02:24 +0700

    Update project_atm.c
    food section update

--- project_atm.c
+++ project_atm.c
@@ -18,10 +18,7 @@ struct snacks_stock{
 struct foods_stock{
     int price;
     char name[50];
-};
-struct drink_stock{
-	int price;
-	char name[50];
+    int qty;
 };
 
 //Declaring global variables
@@ -60,7 +57,8 @@ int menu(){
 //Snacks section
 int snacks(){
     int opt, chance;
-    struct snacks_stock st[10] = {{10, "Lay's Nori Seaweed (Small)"},{15, "Lay's Nori Seaweed (Large)"}};
+    struct snacks_stock st[10];
+    struct snacks_stock st[0] = {10, "Lay's Nori Seaweed (Small)"}, st[1] = {15, "Lay's Nori Seaweed (Large)"};
 
     printf("SNACKS SECTION\n---------------------------------\n");
     printf("Snacks List :\n");
@@ -84,63 +82,10 @@ int snacks(){
     return 0;
 }
 
-//food section
 int food(){
     int foodChoice;
-<<<<<<< HEAD
-    struct foods_stock fst[10] = {{45, "Pork Fried Rice"},{55, "Shrimp Fried Rice"}};
-    
-    printf("FOOD SECTION\n--------------------------------\n");
-    printf("Food List :\n");
-    
-    for(i=0;i<size;i++){
-        printf("Product name :\t\t\t\tPrice:\n%s\t\t\t%d",fst[i].name,fst[i].price);
-=======
-    struct foods_stock fst[size] = {{45, "Pork Fried Rice"}, {55, "Shrimp Fried Rice"}, {69, "Rice with Red Curry"}, {45, "Minced Pork Pad Ka Prao with Fried Egg"}, {40, "Minced Pork Pad Ka Prao"}};
+    struct foods_stock fst[size] = {{45, "Pork Fried Rice"}, {55, "Shrimp Fried Rice"}, {69, "Rice with Red Curry"}, {45, "Minced Pork Pad Ka Prao with Fried Egg"}, {40, "Minced Pork Pad Ka Prao"}, {35, "Smoky flavour pork sausage"}, };
     for(i=0;i<size;i++){
         printf("%s %d\n",fst[i].name,fst[i].price);
->>>>>>> 3f3eb34650f2af0ffd4c65964cfd3376e7849af6
     }
-    
-    printf("\nPlease Select your option (press 0 for exit): ");
-    scanf("%d",&foodChoice);
-    
-    do{
-    	switch(foodChoice){
-    		case 1 : printf("You selected %s (%d baht)",fst[0].name,fst[0].price);
-    		case 2 : printf("You selected %s (%d baht)",fst[1].name,fst[1].price);
-		}
-		printf("\nPlease Select your option (Press 0 for exit): ");
-    	scanf("%d",&foodChoice);
-	}while(foodChoice!=0);
-	
-	return 0;
 }
-
-//drink section
-int drinks(){
-	int drinksChioce;
-	struct drinks_stock dst[10] = {{15,"Cola"},{20,"Oishi"}};
-	
-	printf("DRINKS SECTION\n-------------------------------\n");
-	printf("Drink List :\n");
-	
-	for(i=0;i<size;i++){
-		printf("Product name :\t\t\t\tPrice:\n%s\t\t\t%d",dst[i].name,dst[i].price);
-	}
-	
-	printf("\nPlease Select your option (press 0 for exit): ");
-    scanf("%d",&drinksChoice);
-    
-    do{
-    	switch(drinksChioce){
-    		case 1 :printf("You selected %s (%d baht)",dst[0].name,dst[0].price);
-    		case 2 :printf("You selected %s (%d baht)",dst[1].name,dst[1].price);
-		}
-		printf("\nPlease Select your option (Press 0 for exit): ");
-    	scanf("%d",&drinksChioce);
-	}while(drinksChioce!=0);
-	
-	return 0;
-}
-
